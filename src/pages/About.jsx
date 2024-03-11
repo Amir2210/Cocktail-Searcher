096ebbb73f8265d0a53aa4a44afb87302f5d80b7
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-11 11:35:35 +0200

    Add style to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,10 @@ export function About() {
   return (
     <>
       <Navbar />
-      <h1>im an about page</h1>
+      <section className='page'>
+        <h3>About us</h3>
+        <p className='about-p'>Introducing "MixMaster," the ultimate party sidekick app that fetches cocktails from the hilarious Cocktails DB API. With a flick of your finger, you'll unlock a treasure trove of enchanting drink recipes that'll make your taste buds dance and your friends jump with joy. Get ready to shake up your mixology game, one fantastical mocktail at a time, and let the laughter and giggles flow!</p>
+      </section>
     </>
   )
 }
