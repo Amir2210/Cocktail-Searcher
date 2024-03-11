f430bb9157155f67167c2842bf68e13dbcc6b176
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-03-11 11:42:37 +0200

    Add drink svg to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import { Navbar } from '../cmps/Navbar';
-
+import drink from '../assets/imgs/drink.svg'
 export function About() {
   return (
     <>
@@ -7,6 +7,7 @@ export function About() {
       <section className='page'>
         <h3>About us</h3>
         <p className='about-p'>Introducing "MixMaster," the ultimate party sidekick app that fetches cocktails from the hilarious Cocktails DB API. With a flick of your finger, you'll unlock a treasure trove of enchanting drink recipes that'll make your taste buds dance and your friends jump with joy. Get ready to shake up your mixology game, one fantastical mocktail at a time, and let the laughter and giggles flow!</p>
+        <img className='img about-img' src={drink} alt="drink" />
       </section>
     </>
   )
